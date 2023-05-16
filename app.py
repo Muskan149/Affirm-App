553a55ad8a782e66c713f6fe99914ad3a13b2beb
Muskan149 <muskanmahajan2004@gmail.com>
2023-05-16 12:08:31 +0530

    lots of changes lol

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
+# from affirm import return_affirmations 
+
 # import requests
 
 app = Flask(__name__, template_folder='templates')
