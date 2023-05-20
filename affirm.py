3adb378c9c8714d0bedb7f33b93168d74102837e
Muskan149 <muskanmahajan2004@gmail.com>
2023-05-20 15:48:49 +0530

    there are three files: base, index and login. lfg!

--- affirm.py
+++ affirm.py
@@ -8,28 +8,37 @@ _ = load_dotenv(find_dotenv())  # read local .env file
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 print(openai.api_key)
-# def get_completion(prompt, model="gpt-3.5-turbo"):
-#     messages = [{"role": "user", "content": prompt}]
-#     response = openai.ChatCompletion.create(
-#         model=model,
-#         messages=messages,
-#         temperature=0,  # this is the degree of randomness of the model's output
-#     )
-#     return response.choices[0].message["content"]
 
 
-# def return_affirmations(grievance):
-#     PROMPT = f"""
-#     You are a positive affirmations generator. Your task is to generate empowering affirmations that will help mitigate the user's problems/grievances. 
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0,  # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
 
-#     Generate a Python list of 3 positive affirmations, each affirmation not more than 15 words, for mitigating the user's grievance. The affirmations should not be arbitrary; they should be specific to the user's grievance. 
 
-#     Here is the user's grievance, delimited by three backticks: ```{grievance}```
+def return_affirmations(grievance):
+    PROMPT = f"""
+    You are a positive affirmations generator. Your task is to generate positive, credible, and achievable \ 
+    affirmation statements that will help alleviate the user's problems/grievances.
 
-#     Generate the  affirmations  in the JSON format with the key "three_affirmations"."""
+    Generate a Python list of 3 affirmations, each affirmation not more than 15 words, \
+    for mitigating the user's grievance. The affirmations should not be arbitrary; \
+    they should be specific to the user's grievance.
 
-#     response = get_completion(PROMPT)
-#     response_dict = json.loads(response)
-#     three_affirmations = response_dict["three_affirmations"]
+    Here is the user's grievance, delimited by three backticks: ```{grievance}```
 
-#     return three_affirmations
+    Generate the  affirmations  in the JSON format with the key "affirmations"."""
+
+    response = get_completion(PROMPT)
+    response_dict = json.loads(response)
+    three_affirmations = response_dict["affirmations"]
+
+    return three_affirmations
+
+
+affirmations_list = return_affirmations(
+    "I am suffering from exam related anxiety")
--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template, url_for, request, redirect
-# from affirm import return_affirmations 
+# from affirm import return_affirmations
 
 # import requests
 
 app = Flask(__name__, template_folder='templates')
 
+ # grievance = ""
+    # visible = False
+    # if request.method == 'POST':
+    #     print("POSTED")
+    #     grievance = request.form['grievance']
+    #     visible = True
+    # print(visible)
+    # return render_template("index.html", grievance=grievance, visible=visible)
+
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    grievance = ""
-    visible = False
-    if request.method == 'POST':
-        print("POSTED")
-        grievance = request.form['grievance']
-        visible = True
-    print(visible)
-    return render_template("index.html", grievance=grievance, visible=visible)
+    return render_template("login-sign-up.html")
 
 
 if __name__ == "__main__":
     print("running Affirm")
     app.run(debug=True)
+
